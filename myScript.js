d37d6e7e4bcd1e3eba29ebda1772f4dcd1cabbcb
Milys <mlyszkiewicz@hotmail.com>
2015-04-20 20:26:03 -0700

    Added MyScript.js

---
+++ myScript.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+
+var message = 'Please Respect My Copyright.' ;
+
+    function clickie() {
+    if (document.all)
+    {
+    alert(message);
+    return false;
+     }
+    }
+
+   </SCRIPT>
